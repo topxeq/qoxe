f982e93c5dd75ffc816ad71d6cbef1109e7f7c36
unknown <topxeq@163.com>
2023-04-04 15:09:51 +0800

    2023040403

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,142 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/topxeq/qoxe"
 	"github.com/topxeq/tk"
 )
 
+func runInteractiveShell() int {
+	tk.Pl(`Qoxe V%v`, qoxe.VersionG)
+	qoxe.GlobalsG.Vars["ShellModeG"] = true
+	qoxe.GlobalsG.Vars["leSilentG"] = true
+
+	var following bool
+	var source string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// vm0T := qoxe.NewVM()
+
+	// if tk.IsError(vm0T) {
+	// 	tk.Pl("failed to initialize VM(初始化虚拟机失败): %v", tk.GetErrStrX(vm0T))
+	// 	os.Exit(1)
+	// }
+
+	// vmT := vm0T.(*xie.XieVM)
+
+	// vmT.SetVar(vmT.Running, "argsG", os.Args)
+
+	// guiHandlerG = guiHandler
+
+	// vmT.SetVar(vmT.Running, "guiG", guiHandlerG)
+
+	for {
+		if following {
+			source += "\n"
+			fmt.Print("  ")
+		} else {
+			fmt.Print("> ")
+		}
+
+		if !scanner.Scan() {
+			break
+		}
+		source += scanner.Text()
+		if source == "" {
+			continue
+		}
+
+		if source == "quit" {
+			break
+		} else if source == "#debug" {
+			// vmT.Debug()
+			following = false
+			source = ""
+			continue
+		}
+
+		// retG := ""
+
+		rs := qoxe.RunCode(source, nil)
+
+		tk.Pl("rs: %v", rs)
+
+		// originalCodeLenT := vmT.GetCodeLen(vmT.Running)
+
+		// lrs := vmT.Load(vmT.Running, source)
+
+		// if tk.IsError(lrs) {
+		// 	following = false
+		// 	source = ""
+		// 	fmt.Println("failed to load source code of the script(载入代码失败): ", tk.GetErrStrX(lrs))
+		// 	continue
+		// }
+
+		// rs := vmT.Run(originalCodeLenT)
+
+		// noResultT := tk.IsUndefined(rs) // == "TXERROR:no result")
+
+		// if tk.IsErrX(rs) {
+		// 	fmt.Fprintln(os.Stderr, "failed to run(运行失败): "+tk.GetErrStrX(rs))
+		// 	following = false
+		// 	source = ""
+		// 	continue
+		// }
+
+		// if !noResultT {
+		// 	fmt.Println(retG)
+		// }
+
+		following = false
+		source = ""
+	}
+
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			fmt.Fprintln(os.Stderr, "failed to read char(获取键盘输入失败):", err)
+			return 12
+		}
+	}
+
+	return 0
+}
+
 func main() {
-	rs := qoxe.RunCode("a")
+	codeT := `
+	let five = 5;
+	let ten = 10.2;
+	let b = false;
+	`
+	rs := qoxe.RunCode(codeT, nil)
 
 	tk.Pl("rs: %v", rs)
+
+	return
+
+	// 	codeT := `
+	// 	let five = 5;
+	// let ten = 10;
+	// let add = fn(x, y) {
+	//   x + y;
+	// };
+	// let result = add(five, ten);
+	// !-/*5;
+	// 5 < 10 > 5;
+	// if (5 < 10) {
+	// return true;
+	// } else {
+	// return false;
+	// }
+	// 10 == 10;
+	// 10 != 9;
+	// 	`
+	// 	rs := qoxe.RunCode(codeT, nil)
+
+	// 	tk.Pl("rs: %v", rs)
+
+	// runInteractiveShell()
 }
--- qoxe.go
+++ qoxe.go
@@ -1,5 +1,697 @@
 package qoxe
 
-func RunCode(codeA interface{}) interface{} {
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+
+	"github.com/topxeq/tk"
+)
+
+var VersionG string = "0.0.1"
+
+type GlobalContext struct {
+	SyncMap   tk.SyncMap
+	SyncQueue tk.SyncQueue
+	SyncStack tk.SyncStack
+
+	SyncSeq tk.Seq
+
+	Vars map[string]interface{}
+
+	VerboseLevel int
+}
+
+var GlobalsG *GlobalContext
+
+// token
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func TokenIsEOF(tokenA Token) bool {
+	return tokenA.Type == "EOF"
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+// Lexer
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() Token {
+	var tok Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = Token{Type: EQ, Literal: literal}
+		} else {
+			tok = newToken(ASSIGN, l.ch)
+		}
+	case '+':
+		tok = newToken(PLUS, l.ch)
+	case '-':
+		tok = newToken(MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = Token{Type: NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(SLASH, l.ch)
+	case '*':
+		tok = newToken(ASTERISK, l.ch)
+	case '<':
+		tok = newToken(LT, l.ch)
+	case '>':
+		tok = newToken(GT, l.ch)
+	case ';':
+		tok = newToken(SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(COMMA, l.ch)
+	case '(':
+		tok = newToken(LPAREN, l.ch)
+	case ')':
+		tok = newToken(RPAREN, l.ch)
+	case '{':
+		tok = newToken(LBRACE, l.ch)
+	case '}':
+		tok = newToken(RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType TokenType, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+// ast
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+type LetStatement struct {
+	Token Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type Identifier struct {
+	Token Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
+type ReturnStatement struct {
+	Token       Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+type IntegerLiteral struct {
+	Token Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// parser
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
+type Parser struct {
+	l      *Lexer
+	errors []string
+
+	curToken  Token
+	peekToken Token
+
+	prefixParseFns map[TokenType]prefixParseFn
+	infixParseFns  map[TokenType]infixParseFn
+}
+
+func NewParser(l *Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	p.prefixParseFns = make(map[TokenType]prefixParseFn)
+	p.registerPrefix(IDENT, p.parseIdentifier)
+	p.registerPrefix(INT, p.parseIntegerLiteral)
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) ParseProgram() *Program {
+	program := &Program{}
+	program.Statements = []Statement{}
+
+	for !p.curTokenIs(EOF) {
+		// tk.Pl("curToken: %#v", p.curToken)
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() Statement {
+	switch p.curToken.Type {
+	case LET:
+		return p.parseLetStatements()
+	case RETURN:
+		return p.parseReturnStatements()
+	default:
+		return p.parseExpressionStatement()
+	}
+}
+
+func (p *Parser) parseLetStatements() *LetStatement {
+	stmt := &LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+
+	stmt.Name = &Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatements() *ReturnStatement {
+	stmt := &ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) nextToken() {
+	tk.Pl("curToken: %v", p.curToken)
+
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+
+}
+
+func (p *Parser) curTokenIs(t TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) peekError(t TokenType) {
+	msg := fmt.Sprintf(
+		"expected next token to be %s, got %s instead",
+		t,
+		p.peekToken.Type,
+	)
+	p.errors = append(p.errors, msg)
+}
+
+type (
+	prefixParseFn func() Expression
+	infixParseFn  func(Expression) Expression
+)
+
+func (p *Parser) registerPrefix(tokenType TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpressionStatement() *ExpressionStatement {
+	stmt := &ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() Expression {
+	return &Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() Expression {
+	lit := &IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
+}
+
+// running context
+type RunningContext struct {
+	Resv1 string
+}
+
+type FuncContext struct {
+	Vars map[string]interface{}
+
+	Tmp interface{}
+
+	// ReturnPointer int
+
+	DeferStack *tk.SimpleStack
+
+	// Layer int
+
+	// Parent *FuncContext
+}
+
+func NewFuncContext() *FuncContext {
+	rs := &FuncContext{}
+
+	rs.Vars = make(map[string]interface{})
+
+	rs.DeferStack = tk.NewSimpleStack(10, tk.Undefined)
+
+	return rs
+}
+
+type QoxeVM struct {
+	Regs  []interface{}
+	Stack *tk.SimpleStack
+
+	// Vars map[string]interface{}
+
+	RootFunc *FuncContext
+
+	Running *RunningContext
+}
+
+// func NewVM(inputA ...interface{}) interface{} {
+// 	var inputT interface{} = nil
+
+// 	if len(inputA) > 0 {
+// 		inputT = inputA[0]
+// 	}
+
+// 	rs := &QoxeVM{}
+
+// 	rs.Regs = make([]interface{}, 30)
+// 	rs.Stack = tk.NewSimpleStack(10, tk.Undefined)
+
+// 	rs.RootFunc = NewFuncContext()
+// 	// rs.Vars = make(map[string]interface{}, 0)
+
+// 	var runningT interface{}
+
+// 	if inputT != nil {
+// 		runningT = NewRunningContext(inputT)
+// 	} else {
+// 		runningT = NewRunningContext()
+// 	}
+
+// 	if tk.IsError(runningT) {
+// 		return runningT
+// 	}
+
+// 	rs.Running = runningT.(*RunningContext)
+
+// 	// rs.Running.Parent = rs
+
+// 	// rs.FuncStack = tk.NewSimpleStack(10, tk.Undefined)
+
+// 	// rs.Running.FuncStack.Push(NewFuncContext())
+
+// 	// set global variables
+// 	rs.SetVar(rs.Running, "backQuoteG", "`")
+// 	rs.SetVar(rs.Running, "undefinedG", tk.Undefined)
+// 	rs.SetVar(rs.Running, "nilG", nil)
+// 	rs.SetVar(rs.Running, "newLineG", "\n")
+// 	// rs.SetVar("tmp", "")
+
+// 	return rs
+// }
+
+func init() {
+	// tk.Pl("init")
+
+	// InstrCodeSet = make(map[int]string, 0)
+
+	// for k, v := range InstrNameSet {
+	// 	InstrCodeSet[v] = k
+	// }
+
+	GlobalsG = &GlobalContext{}
+
+	GlobalsG.Vars = make(map[string]interface{}, 0)
+
+	GlobalsG.Vars["backQuote"] = "`"
+
+	GlobalsG.Vars["timeFormat"] = "2006-01-02 15:04:05"
+
+	GlobalsG.Vars["timeFormatCompact"] = "20060102150405"
+
+}
+
+func RunCode(codeA interface{}, ctxA *RunningContext) interface{} {
+
+	l := NewLexer(codeA.(string))
+
+	// for {
+	// 	tok := l.NextToken()
+
+	// 	tk.Pl("token: %#v", tok)
+
+	// 	if TokenIsEOF(tok) {
+	// 		break
+	// 	}
+	// }
+
+	p := NewParser(l)
+
+	tk.Pl("prog: %#v", p)
+
+	program := p.ParseProgram()
+	if program == nil {
+		tk.Fatalf("ParseProgram() returned nil")
+	}
+	errors := p.Errors()
+	if len(errors) > 0 {
+		tk.Pl("parser has %d errors", len(errors))
+		for _, msg := range errors {
+			tk.Pl("parse error: %q", msg)
+		}
+
+		return ""
+	}
+
+	// if len(program.Statements) != 3 {
+	// 	tk.Fatalf(
+	// 		"program.Statements does not contain 3 statements. got=%d",
+	// 		len(program.Statements),
+	// 	)
+	// }
+
+	tk.Pl("prog: %#v", program)
+
+	for i, v := range program.Statements {
+		tk.Pl("statement[%v]: %#v", i, v)
+	}
+
 	return ""
 }
